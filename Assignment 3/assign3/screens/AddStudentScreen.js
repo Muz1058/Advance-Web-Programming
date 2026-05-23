c35a383d3c6258fab7e734bea0e494294184f5bd
@Ali.Muzaffar <muzaffarali3633@gmail.com>
2026-05-24 01:30:59 +0500

    Add student form with inputs and state

--- Assignment 3/assign3/screens/AddStudentScreen.js
+++ Assignment 3/assign3/screens/AddStudentScreen.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
 
-import { View,Text,Button } from "react-native";
+import { View,Text,Button, ScrollView, TextInput } from "react-native";
 
 const AddStudentScreen=({navigation})=>{
-    <View>
-        <Text>Welcome Home</Text>
-    </View>
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [phone,setPhone]=useState("")
+    const [City,setCity]=useState("")
+
+    
+
+
+
+   return(
+     <ScrollView>
+        <SafeAreaView>
+        <Text>Name</Text>
+        <TextInput
+        value="{name}"
+        onChange={setName}
+        placeholder="Enter Name"
+        />
+        <Text>Email</Text>
+        <TextInput
+        value="{email}"
+        onChange={setEmail}
+        placeholder="Enter Email"
+        />
+        <Text>Name</Text>
+        <TextInput
+        value="{Phone}"
+        onChange={setPhone}
+        placeholder="Enter Phone No."
+        />
+        <Text>Name</Text>
+        <TextInput
+        value="{City}"
+        onChange={setCity}
+        placeholder="Enter City"
+        />
+
+        </SafeAreaView>
+       
+    </ScrollView>
+   )
 
 }
 
